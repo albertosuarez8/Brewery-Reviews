32e5a0672d6baf7088beb3db06b2f07cd3b48ed0
Albert <alberto.suarez8@outlook.com>
2023-01-23 16:50:04 -0500

    Image to DB functionality

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,16 +1,28 @@
 const router = require('express').Router();
 const { Post, User } = require('../../models');
 
+router.get('/post', async (req, res) => {
+    try {
+        let x = await Post.findAll();
+        x.map(x=> {
+            x.image = Buffer.from(x.image).toString('base64');
+        })
+        res.status(200).json(x);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 router.post('/', async (req, res) => {
     try {
-        const user = await User.findOne({ where: { username: req.session.email } });
+        // const user = await User.findOne({ where: { username: req.session.email } });
         const postData = await Post.create({
             description: req.body.description,
             stadium: req.body.stadium,
             rating: req.body.rating,
             section: req.body.section,
-            image: req.body.image,
-            user_id: user.id,
+            image: Buffer.from(req.body.image, 'base64'),
+            // user_id: user.id,
             date: new Date(),
         });
         res.status(200).json(postData);
@@ -29,7 +41,7 @@ router.put('/update', async (req, res) => {
             rating: req.body.rating,
             section: req.body.section,
             image: req.body.image,
-            user_id: user.id,
+            // user_id: user.id,
             date: new Date(),
         });
         res.status(200).json(post);
