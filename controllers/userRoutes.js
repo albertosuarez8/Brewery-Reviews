bc0e129b43f9be5ec5ef10169be464546a8118a4
jujubea22 <114043890+jujubea22@users.noreply.github.com>
2023-01-23 21:05:03 -0500

    create user routes

---
+++ controllers/userRoutes.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const { User } = require('models');
+const sequelize = require('./controllers/api');
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: reg,body,email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'email or password not recognized, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'Thanks for logging in' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
